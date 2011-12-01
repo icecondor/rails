21a8f8540f567744ba03edbdb0cb0565f688e53a
Don Park <don@donpark.org>
2011-12-01 13:56:34 -0800

    skeleton rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Icecondor::Application.config.secret_token = 'f98f1cdd3bb4b0a9a5aacb407a3e5c01e454df8f52a581f9eb2fd0dcf5b102f5c4160f35a4268588927973262a44c9f2e0f97514c274d5cd19083a4d01101887'
