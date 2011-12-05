bd8739473f24bb43bc0925fc1971d6cc874410a4
Don Park <don@donpark.org>
2011-12-05 14:16:02 -0800

    start the search for users

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,10 @@
 class Location < CouchRest::Model::Base
+  design do
+    view :last_reports_for_user,
+         :map => "function(doc) {
+             if(doc.type == 'location') {
+               emit([doc.user,doc.date],doc);
+             }
+           }"
+  end
 end
