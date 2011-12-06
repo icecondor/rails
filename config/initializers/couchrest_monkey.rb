8be4472ae00fa30e6f179ca420b5efc3a60c5719
Don Park <don@donpark.org>
2011-12-05 16:15:22 -0800

    use underscore on document creation, too

--- config/initializers/couchrest_monkey.rb
+++ config/initializers/couchrest_monkey.rb
@@ -1,10 +1,27 @@
 module CouchRest
   module Model
+    class Base < CouchRest::Document
+      def initialize(attributes = {}, options = {})
+        super()
+        prepare_all_attributes(attributes, options)
+        # set the instance's database, if provided
+        self.database = options[:database] unless options[:database].nil?
+        unless self['_id'] && self['_rev']
+          self[self.model_type_key] = self.class.to_s.underscore
+        end
+
+        yield self if block_given?
+
+        after_initialize if respond_to?(:after_initialize)
+        run_callbacks(:initialize) { self }
+      end
+
+    end
     module Persistence
       module ClassMethods
         def build_from_database(doc = {}, options = {}, &block)
           src = doc[model_type_key]
-          base = (src.blank? || src.camelize == self.to_s) ? self : src.constantize
+          base = (src.blank? || src.camelize == self.to_s) ? self : src.camelize.constantize
           base.new(doc, options.merge(:directly_set_attributes => true), &block)
         end
       end
