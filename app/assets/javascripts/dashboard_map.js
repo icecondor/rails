f5e1122ad8e0cc6c9966614f2c76bf12236d0d1f
Don Park <don@donpark.org>
2011-12-05 17:04:10 -0800

    put the js pieces together

--- app/assets/javascripts/dashboard_map.js
+++ app/assets/javascripts/dashboard_map.js
@@ -1,26 +1,31 @@
 var map, bounds;
 var tracked = {};
-var socketio = io.connect();
-socketio.emit('following', { type: 'follow', username: 'iss' });
 
-//socketio.emit('following', { type: 'follow', username: '<%=ary.last.user.username%>' });
+$(mapstart);
+$(iostart);
 
-socketio.on('update', function(data) {
-  console.log(data)
-  if(data.type=="location") {
-    if(tracked[data.username]) {
-      var point = new google.maps.LatLng(data.position.latitude, 
-                                         data.position.longitude);
-      tracked[data.username].setPosition(point);
-      $('#'+data.username+'-date').update(data.date)
-    } else {
-      tracked[data.username] = make_marker(data.position);
-      $('#trackedlist').insert("<tr><td><img src='/images/gmaps/mm_20_red.png'></td><td><a href='/"+data.username+"'>"+data.username+"</a></td><td id='"+data.username+"-date'>"+data.date+"</td></tr>")
+function iostart() {
+  var socketio = io.connect();
+  socketio.emit('following', { type: 'follow', username: 'iss' });
+
+  //socketio.emit('following', { type: 'follow', username: '<%=ary.last.user.username%>' });
+
+  socketio.on('update', function(data) {
+    console.log(data)
+    if(data.type=="location") {
+      if(tracked[data.username]) {
+        var point = new google.maps.LatLng(data.position.latitude, 
+                                           data.position.longitude);
+        tracked[data.username].setPosition(point);
+        $('#'+data.username+'-date').update(data.date)
+      } else {
+        tracked[data.username] = make_marker(data.position);
+        $('#trackedlist').insert("<tr><td><img src='/images/gmaps/mm_20_red.png'></td><td><a href='/"+data.username+"'>"+data.username+"</a></td><td id='"+data.username+"-date'>"+data.date+"</td></tr>")
+      }
     }
-  }
-})
+  })
+}
 
-document.observe("dom:loaded",mapstart);
 function mapstart() {
   bounds = new google.maps.LatLngBounds();
   var mapOptions = {
@@ -31,7 +36,6 @@ function mapstart() {
   map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
   map.fitBounds(bounds);
 
-  add_fixed_users()
 }
 
 function add_user() {
