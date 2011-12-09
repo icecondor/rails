6e1cd40ae952dc436b92cda3380c5fe62bf084f3
Don Park <don@donpark.org>
2011-12-09 11:04:58 -0800

    developers page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Icecondor::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
+  match "/dashboard/livemap" => "dashboard#map"
   match "/dashboard/features" => "dashboard#features"
+  match "/dashboard/developers" => "dashboard#developers"
+  
   root :to => 'dashboard#map'
 end
