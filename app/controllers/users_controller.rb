55b932264e5cc1af17bb9e08a76213a3a670df1a
Don Park <don@donpark.org>
2011-12-09 16:08:31 -0800

    screams for refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def create
-    render :json => {:status => "OK"}
+    u_e = User.find_by_email(params[:email])
+    u_u = User.find_by_username(params[:username])
+    response = {}
+    response.merge!({:email => "TAKEN"}) if u_e
+    response.merge!({:username => "TAKEN"}) if u_u
+    if u_e.nil? && u_u.nil?
+      user = User.create(params)
+      response.merge!({:status => "OK"})
+    else
+      response.merge!({:status => "ERR"})
+    end
+    render :json => response
   end
 end
