1fa39b8b9da28f86cddb2c3d1b10720aebfa1725
Don Park <don@donpark.org>
2011-12-14 11:25:42 -0800

    add the bird to Login

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   def map
     last_usernames = Location.last_users
     @locations = last_usernames.map {|username| Location.last_for(username, 1).first}
+    logger.info last_usernames.inspect
     @group = last_usernames.inject({}) do |hash, username|
       user = User.find_by_username(username)
       hash[username] = user || {}
