f9ae66cf732ded707c51cecb4f7a6f0d7a4104bb
Don Park <don@donpark.org>
2011-12-19 16:14:38 -0800

    slip it a history count

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,11 @@
 class DashboardController < ApplicationController
   def map
+    count = params[:count]
     last_usernames = Location.last_users
     @group = last_usernames.inject({}) do |hash, username|
       user = User.find_by_username(username)
       hash[username] = user || {}
-      hash[username][:locations] = Location.last_for(username, 10)
+      hash[username][:locations] = Location.last_for(username, count || 10)
       hash[username][:markers] = []
       hash
     end
