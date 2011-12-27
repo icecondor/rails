2bb6c30099ea8a87c41a9608e68e47cab2f7190e
Don Park <don@donpark.org>
2011-12-27 11:32:58 -0800

    more asset help

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ Icecondor::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
+
 end
