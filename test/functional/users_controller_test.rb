29862bba0dd1de60686f870957a6bd328eb77c45
Don Park <don@donpark.org>
2012-01-03 08:59:27 -0800

    lets get these test rolling

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "create" do
+
+    post :create
+    assert_response :success
+  end
 end
