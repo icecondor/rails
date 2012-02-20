75b387e45107fcbe1eb021001e6924cd36a76cbc
Don Park <don@donpark.org>
2012-02-20 13:03:28 +0700

    test fix for v1locations (not used anymore)

--- test/functional/locations_controller_test.rb
+++ test/functional/locations_controller_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class LocationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-  test "location creation" do    
-    post :create
+  test "location v1 creation" do
+    location = { timestamp: "2012-02-02",
+                 batterylevel: 40,
+                 provider: "gps",
+                 heading: 120,
+                 velocity: 10,
+                 position: { latitude: 45,
+                             longitude: -122,
+                             accuracy: 100,
+                             altitude: 100}}
+    post :create, { :location => location,
+                    :client => {:version => "20100606"}}
     assert_response :success
   end
 end
